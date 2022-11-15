e0449b4d5a4dbdd8c19cc5baea42a14c37cbe781
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2022-11-15 19:25:50 -0300

    Adicionado input de imagem por URL

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // getFormData = (selector) => Object.fromEntries(new FormData(document.querySelector(selector)))
-const foto = document.getElementById('card__image')
+const imagem = document.getElementById('card__image')
 const bike = document.getElementById('bike-name')
 const velocidade = document.getElementById("Km/h")
 const potencia = document.getElementById("HP")
@@ -8,9 +8,9 @@ const cilindradas = document.getElementById("Cilindradas")
 const cilindros = document.getElementById("Cilindros")
 const peso = document.getElementById("Peso")
 
-console.log(foto.value)
 // Cria array com elementos HTML para extração do valor
 const array_html_elements = [
+    imagem,
     bike,
     velocidade,
     potencia,
@@ -27,8 +27,8 @@ array_html_elements.forEach(element => element.addEventListener("keyup", functio
     
     // Cria array com os valores das tags de input
     array_data_values = array_html_elements.map(e => { return e.value })
-    
-    // TODO CRIAR REGRA PARA INPUT COM LETRAS
+
+    // CRIAR REGRA PARA INPUT COM LETRAS
     /* if (array_data_values.some((element) => /^\d+$/.test(element) != true)) {
         alert("Digite apenas números")
     }*/
@@ -54,8 +54,8 @@ section.innerHTML += `<card id="card">
                             <div class="card__cod">C3</div>
                             <div class="card__title">Motos Custom</div>
                         </div>
-                        <img id="card__image" src="${foto.src}" alt="">
-                        <h2 id="card__bike-name">${array_data_values[0]}</h2>
+                        <img id="card__image" src="${array_data_values[0]}" alt="">
+                        <h2 id="card__bike-name">${array_data_values[1]}</h2>
                             <div id="card__data">
                                 <div id="card__feature-label">
                                     <label for="Km/h">Km/h</label>
@@ -66,12 +66,12 @@ section.innerHTML += `<card id="card">
                                     <label for="Peso">Peso</label>
                                 </div>
                                 <div id="card_copy__feature-value">
-                                    <small id="Km/h" class="card-value">${array_data_values[1]}</small>
-                                    <small id="HP" class="card-value">${array_data_values[2]}</small>
-                                    <small id="RPM" class="card-value">${array_data_values[3]}</small>
-                                    <small id="Cilindradas" class="card-value">${array_data_values[4]}</small>
-                                    <small id="Cilindros" class="card-value">${array_data_values[5]}</small>
-                                    <small id="Peso" class="card-value">${array_data_values[6]}</small>
+                                    <small id="Km/h" class="card-value">${array_data_values[2]}</small>
+                                    <small id="HP" class="card-value">${array_data_values[3]}</small>
+                                    <small id="RPM" class="card-value">${array_data_values[4]}</small>
+                                    <small id="Cilindradas" class="card-value">${array_data_values[5]}</small>
+                                    <small id="Cilindros" class="card-value">${array_data_values[6]}</small>
+                                    <small id="Peso" class="card-value">${array_data_values[7]}</small>
                                 </div>
                             </div>
                     </card>`
