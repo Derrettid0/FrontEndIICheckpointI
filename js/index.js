c52336d11cd33125ee7fef0211fd9728a6a32bf7
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2022-11-15 12:23:51 -0300

    Criado cards filho

--- js/index.js
+++ js/index.js
@@ -1,168 +1,82 @@
-    // getFormData = (selector) => Object.fromEntries(new FormData(document.querySelector(selector)))
-    const foto = document.getElementById('urlFoto')
-    const velocidade = document.getElementById("Km/h")
-    const potencia = document.getElementById("HP")
-    const rpm = document.getElementById("RPM")
-    const cilindradas = document.getElementById("Cilindradas")
-    const cilindros = document.getElementById("Cilindros")
-    const peso = document.getElementById("Peso")
+// getFormData = (selector) => Object.fromEntries(new FormData(document.querySelector(selector)))
+const foto = document.getElementById('card__image')
+const bike = document.getElementById('bike-name')
+const velocidade = document.getElementById("Km/h")
+const potencia = document.getElementById("HP")
+const rpm = document.getElementById("RPM")
+const cilindradas = document.getElementById("Cilindradas")
+const cilindros = document.getElementById("Cilindros")
+const peso = document.getElementById("Peso")
 
-    // Cria array com elementos HTML para extração do valor
-    const array_html_elements = [
-        foto,
-        velocidade,
-        potencia,
-        rpm,
-        cilindradas,
-        cilindros,
-        peso,
-    ]
+console.log(foto.value)
+// Cria array com elementos HTML para extração do valor
+const array_html_elements = [
+    bike,
+    velocidade,
+    potencia,
+    rpm,
+    cilindradas,
+    cilindros,
+    peso,
+]
 
-    let array_data_values = []
+// Cria array para armazenar os valores dos inputs
 
-    // Habilita botão submit somente se todos campo estiverem preenchidos
-    array_html_elements.forEach(element => element.addEventListener("keyup", function () {
-
-        // Cria array com os valores das tags de input
-        array_data_values = array_html_elements.map(e => { return e.value })
-
-        // if (array_data_values.some((element) => /^\d+$/.test(element) != true)) {
-        //     alert("Digite apenas números")
-        // }
-
-        if (array_data_values.every((element) => element != "")) {
-            const btn = document.getElementById("btn")
-            btn.removeAttribute("disabled")
-        } else {
-            btn.setAttribute("disabled", true)
-        }
-
-    }));
-
-
-    let submit = document.getElementById("btn").addEventListener("click", function () {
-        // Cria array com os valores das tags de input
-        array_data_values = array_html_elements.map(e => { return e.value })
-
-        console.log(array_data_values);
-
-    });
-
-let sectionConteudos = document.querySelector('.criados')
-
-const url = array_data_values
-
-function pegarDados(i) {
-    fetch(url)
-      .then( response => response.json() )
-      .then( dados => {
-          if(dados.erro) {
-              console.log("Erro ao acessar o JSON")
-              return
-          }
-          atribuirDados(dados, i)
-      })};
-
-function atribuirDados(dados, i){
-foto.setAttribute('src', dados.array_data_values[i].urlFoto)
-velocidade.textContent = dados.array_data_values[i].velocidade
-potencia.textContent = dados.array_data_values[i].potencia
-rpm.textContent = dados.array_data_values[i].rpm
-cilindradas = dados.array_data_values[i].cilindradas
-cilindros = dados.array_data_values[i].cilindros
-peso = dados.array_data_values[i].peso
-
-}
-
-function desenharCarta(id){
-    let carta = document.createElement('card')
-    carta.setAttribute('class', 'card')
-    carta.setAttribute('id', 'card')
-    sectionConteudos.appendChild(carta)
-
-    let titulo = document.createElement ('h1')
-    titulo.setAttribute('id', 'card__title')
-    carta.appendChild(titulo)
+// Habilita botão submit somente se todos os campos estiverem preenchidos
+array_html_elements.forEach(element => element.addEventListener("keyup", function () {
     
-    let imagem = document.createElement('img')
-    imagem.setAttribute('src', 'https://picsum.photos/150/100')
-    carta.appendChild(imagem)
-
-    let modelo = document.createElement('h2')
-    modelo.setAttribute('id', 'card__bike-name')
-    carta.appendChild(modelo)
-
-    let container = document.createElement('div')
-    container.setAttribute('id', 'criado')
-    carta.appendChild(container)
-
-    let label = document.createElement ('div')
-    label.setAttribute('id', 'card__feature-label')
-    container.appendChild(label)
-
-    let lbl = document.createElement ('label')
-    lbl.setAttribute('for', 'Km/h')
-    lbl.textContent = 'Km/h'
-    label.appendChild(lbl)
-
-    let lbl1 = document.createElement ('label')
-    lbl1.setAttribute('for', 'HP')
-    lbl1.textContent = 'HP'
-    label.appendChild(lbl1)
-
-    let lbl2 = document.createElement ('label')
-    lbl2.setAttribute('for', 'RPM')
-    lbl2.textContent = 'RPM'
-    label.appendChild(lbl2)
-
-    let lbl3 = document.createElement ('label')
-    lbl3.setAttribute('for', 'Cilindradas')
-    lbl3.textContent = 'Cilindradas'
-    label.appendChild(lbl3)
-
-    let lbl4 = document.createElement ('label')
-    lbl4.setAttribute('for', 'Cilindros')
-    lbl4.textContent = 'Cilindros'
-    label.appendChild(lbl4)
-
-    let lbl5 = document.createElement ('label')
-    lbl5.setAttribute('for', 'Peso')
-    lbl5.textContent = 'Peso'
-    label.appendChild(lbl5)
-
-    let cardValue = document.createElement('div')
-    cardValue.setAttribute('id','card__feature-value')
-    container.appendChild(cardValue)
-
-    let speed = document.createElement('p')
-    speed.setAttribute('id','Km/h')
-    speed.setAttribute('class', 'card-value')
-    cardValue.appendChild(speed)
+    // Cria array com os valores das tags de input
+    array_data_values = array_html_elements.map(e => { return e.value })
     
-    let horsePower = document.createElement('p')
-    horsePower.setAttribute('id','HP')
-    horsePower.setAttribute('class', 'card-value')
-    cardValue.appendChild(horsePower)
-
-    let rotacoes = document.createElement('p')
-    rotacoes.setAttribute('id','RPM')
-    rotacoes.setAttribute('class', 'card-value')
-    cardValue.appendChild(rotacoes)
-
-    let cc = document.createElement('p')
-    cc.setAttribute('id','Cilindradas')
-    cc.setAttribute('class', 'card-value')
-    cardValue.appendChild(cc)
-
-    let Cilind = document.createElement('p')
-    Cilind.setAttribute('id','Km/h')
-    Cilind.setAttribute('class', 'card-value')
-    cardValue.appendChild(Cilind)
-
-    let massa = document.createElement('p')
-    massa.setAttribute('id','Km/h')
-    massa.setAttribute('class', 'card-value')
-    cardValue.appendChild(massa)
+    // TODO CRIAR REGRA PARA INPUT COM LETRAS
+    /* if (array_data_values.some((element) => /^\d+$/.test(element) != true)) {
+        alert("Digite apenas números")
+    }*/
     
-    pegarDados(i)
-}   
+    if (array_data_values.every((element) => element != "")) {
+        const btn = document.getElementById("btn")
+        btn.removeAttribute("disabled")
+    } else {
+        btn.setAttribute("disabled", true)
+    }
+}));
+
+let array_data_values = []
+
+const section = document.getElementById('section')
+
+document.getElementById("btn").addEventListener("click", function () {
+// Cria array com os valores do input para popular o novo card.
+array_data_values = array_html_elements.map(e => { return e.value })
+// Faz o append do HTML do card criado.
+section.innerHTML += `<card id="card">
+                        <div class="card__header">
+                            <div class="card__cod">C3</div>
+                            <div class="card__title">Motos Custom</div>
+                        </div>
+                        <img id="card__image" src="${foto.src}" alt="">
+                        <h2 id="card__bike-name">${array_data_values[0]}</h2>
+                            <div id="card__data">
+                                <div id="card__feature-label">
+                                    <label for="Km/h">Km/h</label>
+                                    <label for="HP">HP</label>
+                                    <label for="RPM">RPM</label>
+                                    <label for="Cilindradas">Cilindradas</label>
+                                    <label for="Cilindros">Cilindros</label>
+                                    <label for="Peso">Peso</label>
+                                </div>
+                                <div id="card_copy__feature-value">
+                                    <small id="Km/h" class="card-value">${array_data_values[1]}</small>
+                                    <small id="HP" class="card-value">${array_data_values[2]}</small>
+                                    <small id="RPM" class="card-value">${array_data_values[3]}</small>
+                                    <small id="Cilindradas" class="card-value">${array_data_values[4]}</small>
+                                    <small id="Cilindros" class="card-value">${array_data_values[5]}</small>
+                                    <small id="Peso" class="card-value">${array_data_values[6]}</small>
+                                </div>
+                            </div>
+                    </card>`
+
+// Apaga os valores do card após o submit
+array_html_elements.forEach(e => e.value = '')
+});
+
